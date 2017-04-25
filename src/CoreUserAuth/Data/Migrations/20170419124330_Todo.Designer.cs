1637d7a1f6d080f7a63c6d7ba90d972f72c4b5ee
Niels Odgaard <nod@novicell.dk>
2017-04-25 13:30:51 +0200

    Updated to VS2017

--- src/CoreUserAuth/Data/Migrations/20170419124330_Todo.Designer.cs
+++ src/CoreUserAuth/Data/Migrations/20170419124330_Todo.Designer.cs
@@ -8,9 +8,10 @@ using CoreUserAuth.Data;
 namespace CoreUserAuth.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170419124330_Todo")]
+    partial class Todo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
